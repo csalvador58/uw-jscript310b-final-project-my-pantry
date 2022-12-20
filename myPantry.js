aca3f5e7852155bae7e24f1e193005b0e30e7390
Chris Salvador <csalvador58@gmail.com>
2022-12-19 20:31:26 -0800

    Refactored event listeners

--- myPantry.js
+++ myPantry.js
@@ -68,15 +68,15 @@
                 nutrition: null
             }
 
-            if(this.ingredients.some(ingredient => ingredient.name.includes(ingredientName))) {
+            if (this.ingredients.some(ingredient => ingredient.name.includes(ingredientName))) {
                 // Lookup object name in pantry array
                 for (let ingredient of this.ingredients) {
-                    if(ingredient.name === ingredientName) {
+                    if (ingredient.name === ingredientName) {
                         ingredient.qty = qty;
                         // Prompt user to confirm unit of measurement if already exists in database.
-                        if(ingredient.unit !== unit) {
+                        if (ingredient.unit !== unit) {
                             let response = confirm(`The unit of measurement for "${ingredientName}" already exists in the database. Do you want to update this from "${ingredient.unit}" to "${unit}"`);
-                            if(response) {
+                            if (response) {
                                 ingredient.unit = unit;
                                 alert(`The unit of measurement for ${ingredientName} has been updated to "${unit}".`)
                             } else alert(`The unit of measurement for ${ingredientName} was not updated.`)
@@ -124,145 +124,153 @@
     document.addEventListener('blur', processInputValidationChecks, true);
     
     function processInputValidationChecks(e) {
-        if( ['item-name', 'recipe-name', 'recipe-item'].includes(e.target.id) ) {
+        if ( ['item-name', 'recipe-name', 'recipe-item'].includes(e.target.id) ) {
             isInputInvalid(e.target, e.target.value, 0);  
         } 
-        if( ['quantity', 'recipe-item-qty' ].includes(e.target.id) ) {
+        if ( ['quantity', 'recipe-item-qty' ].includes(e.target.id) ) {
             isInputInvalid(e.target, e.target.value, 1);
         } 
-        if( ['unit-of-measure', 'recipe.item-unit'].includes(e.target.id) ) {
+        if ( ['unit-of-measure', 'recipe.item-unit'].includes(e.target.id) ) {
             isInputInvalid(e.target, e.target.value, 2);
         } 
     }
 
-    // Delete ingredients from pantry array. 
-    // Add to recipe from pantry list to populate name and unit of ingredient when building a recipe.
-    pantryList.addEventListener('click', function(e) {
-        if(e.target.classList.contains('delete')) e.target.parentElement.remove();
+    // Event listener to process 'add to recipe' and 'delete' actions.
+    pantryList.addEventListener('click', pantryButtonClick);
+
+    function pantryButtonClick(e) {
+        if (e.target.classList.contains('delete')) e.target.parentElement.remove();
+
         for (let ingredient of pantry) {
-            if(ingredient.name === e.target.id) ingredient.delete();
+            if (ingredient.name === e.target.id) ingredient.delete();
         }
 
-        if(e.target.classList.contains('add-to-recipe')) {
-            const pantryLookup = pantry.find(item => item.name === e.target.previousElementSibling.id);
-                
+        if (e.target.classList.contains('add-to-recipe')) {
+            const pantryLookup = pantry.find(item => item.name === e.target.previousElementSibling.id); 
             recipeItem.value = pantryLookup.name;
             recipeItemUnit.value = pantryLookup.unit;
             resetInputsExceptFor('recipe-item', 'recipe-item-unit');
         }
 
         displayPantry();
-    });
+    }
 
-    // Delete ingredient from list when building a recipe.
-    ingredientsList.addEventListener('click', function(e) {
-
-        if(e.target.classList.contains('delete')) {
+    // Remove ingredient from temp recipe on 'delete' click
+    ingredientsList.addEventListener('click', removeListItem);
+    
+    function removeListItem(e) {
+        if (e.target.classList.contains('delete')) {
             e.target.parentElement.remove();
             for (let ingredient of tempRecipe.ingredients) {
-                if(ingredient.name === e.target.id) tempRecipe.delete(ingredient);
+                if (ingredient.name === e.target.id) tempRecipe.delete(ingredient);
             }
         }
-
+    
         displayInputRecipe();
-    });
+    }
 
     // Delete a recipe from Recipe list section.
     // Show list of ingredients for a recipe listed.
-    recipeList.addEventListener('click', function(e) {   
-        
-        // Call function to fetch data from API
-        if(e.target.classList.contains('view-nutrition')) {
+    recipeList.addEventListener('click', recipeButtonClick);
+    
+    function recipeButtonClick(e) {
+        const request = e.target;
 
-            getIngredientData(e.target.nextElementSibling.id);
+        // Call function to fetch data from API when data button is clicked
+        if (request.classList.contains('view-nutrition')) {
+            getIngredientData(request.nextElementSibling.id);
         }
-
-        // Map method used to return array of recipe names from object array. Splice and indexOf methods use to find location of specific recipe and remove from array.
-        if(e.target.classList.contains('delete')) {
-            e.target.parentElement.remove();
+    
+        // Remove recipe from list and from recipes array using splice method when delete button is clicked
+        if (request.classList.contains('delete')) {
+            request.parentElement.remove();
             // Remove recipe from recipes array
-            recipes.splice(recipes.findIndex(recipe => recipe.name === e.target.id), 1);
+            recipes.splice(recipes.findIndex(recipe => recipe.name === request.id), 1);
             displayRecipes();
         }
-
-        if(e.target.classList.contains('view-recipe')) {
+    
+        // Display or remove recipe ingredients when view or hide button is clicked
+        if (request.classList.contains('view-recipe')) {
             
-            if (e.target.parentElement.classList.contains('recipe-view-enabled')) {
-                e.target.innerText = 'View';
-                e.target.parentElement.classList.remove('recipe-view-enabled');
-                e.target.nextElementSibling.remove();
-
+            if (request.parentElement.classList.contains('recipe-view-enabled')) {
+                request.innerText = 'View';
+                request.parentElement.classList.remove('recipe-view-enabled');
+                request.nextElementSibling.remove();
+    
             } else {
-                e.target.parentElement.classList.add('recipe-view-enabled');
-                e.target.innerText = 'Hide';
-
+                request.parentElement.classList.add('recipe-view-enabled');
+                request.innerText = 'Hide';
+    
                 let newUL = document.createElement('ul');
-                
-                let recipeLookup = recipes.find(recipe => recipe.name === e.target.previousElementSibling.id);
+                let recipeLookup = recipes.find(recipe => recipe.name === request.previousElementSibling.id);
+
                 for (let ingredient of recipeLookup.ingredients) {
                     let newListItem = document.createElement('li');
                     newListItem.classList.add('tab');
                     newListItem.innerHTML = `\n• ${ingredient.name} ${ingredient.qty} ${ingredient.unit}\n`;
                     newUL.appendChild(newListItem);
                 }
-                e.target.parentElement.appendChild(newUL);
+                request.parentElement.appendChild(newUL);
             }
         } 
-        
-    });
+    }
 
-        // Add an item to pantry.
-    pantryInput.addEventListener('click', function(e) {
-
-         
+    // Add an item to pantry.
+    pantryInput.addEventListener('click', addItemToPantryList);
+    
+    function addItemToPantryList(e) {
+        // Complete form input checks
         isInputInvalid(pantryItemName, pantryItemName.value, 0);
         isInputInvalid(pantryQuantity, pantryQuantity.value, 1);
         isInputInvalid(pantryMeasureUnit, pantryMeasureUnit.value, 2);
-
-        if(! (pantryItemName.validity.customError + pantryQuantity.validity.customError + pantryMeasureUnit.validity.customError ) > 0 ) {
-            
+    
+        if (! (pantryItemName.validity.customError + pantryQuantity.validity.customError + pantryMeasureUnit.validity.customError ) > 0 ) {
             addToPantry(pantryItemName.value, parseFloat(pantryQuantity.value), pantryMeasureUnit.value);
-
             resetInputsExceptFor();
         }
-        
-    });
+    }
+
 
     // Add an ingredient to a temp recipe array used for recipe building.
-    recipeInput.addEventListener('click', function(e) {   
-        
+    recipeInput.addEventListener('click', addIngredientToTempRecipe);
+    
+    function addIngredientToTempRecipe(e) {
+        // Complete form input checks
         isInputInvalid(recipeItem, recipeItem.value, 0);
         isInputInvalid(recipeItemQty, recipeItemQty.value, 1);
         isInputInvalid(recipeItemUnit, recipeItemUnit.value, 2);
+    
+        if (! (recipeItem.validity.customError + recipeItemQty.validity.customError + recipeItemUnit.validity.customError ) > 0 ) {
 
-        if(! (recipeItem.validity.customError + recipeItemQty.validity.customError + recipeItemUnit.validity.customError ) > 0 ) {
-            
+            // Add recipe to tempRecipe object using prototype method
             tempRecipe.add(recipeItem.value, recipeItemQty.value, recipeItemUnit.value)
-
+    
             displayInputRecipe();
             resetInputsExceptFor('recipe-name');
         }
-    });
+    }
 
     
     // Copy ingredients from temp recipe and create a new recipe object to store.
-    recipeSubmit.addEventListener('click', function(e) {
-
-        // Run validity check
+    recipeSubmit.addEventListener('click', saveRecipe);
+    
+    function saveRecipe(e) {
+        // Run validity checks
         isInputInvalid(recipeName, recipeName.value, 0);
-        if(tempRecipe.ingredients.length < 1) alert('Please add ingredients to create a recipe.');
-
+        if (tempRecipe.ingredients.length < 1) alert('Please add ingredients to create a recipe.');
+    
         // Check validity error and ensure recipe list is not empty before creating new Recipe object
-        if(! (recipeName.validity.customError + (tempRecipe.ingredients.length < 1)) > 0 ) {
+        if (! (recipeName.validity.customError + (tempRecipe.ingredients.length < 1)) > 0 ) {
             const newRecipe = new Recipe(recipeName.value);
     
             for(let ingredient of tempRecipe.ingredients) {
+                // Add recipe to newRecipe object using prototype method
                 newRecipe.add(ingredient.name, ingredient.qty, ingredient.unit);
             }
-    
+            // Store new recipe in recipes array
             recipes.push(newRecipe);
     
-            // Clear temp recipe
+            // Clear temp recipe for next build
             tempRecipe.name = null;
             tempRecipe.ingredients = [];
             
@@ -270,79 +278,66 @@
             resetInputsExceptFor();
             displayRecipes();
         }
+    }
 
-    });
+    // Save, load, erase event listeners
+    saveData.addEventListener('click', processSaveLoadErase);
+    loadData.addEventListener('click', processSaveLoadErase);
+    eraseData.addEventListener('click', processSaveLoadErase);
+    
+    function processSaveLoadErase(e) {
+        if (e.target === saveData) {
+            const pantryData = JSON.stringify(pantry);
+            const recipeData = JSON.stringify(recipes);
 
-    // JSON.stringify turns an object to a string
-    // JSON.parse turns a string to an object
-    // localStorage.setItem(key, value);
-
-    // Save pantry and recipe data to local storage.
-    saveData.addEventListener('click', function(e) {
-
-        const pantryData = JSON.stringify(pantry);
-        const recipeData = JSON.stringify(recipes);
-
-        localStorage.setItem('pantryData', pantryData);
-        localStorage.setItem('recipeData', recipeData);
-        
-        resetInputsExceptFor();
-        displayPantry();
-        displayRecipes();
-    });
-
-    // Load pantry and recipe data from local storage.
-    loadData.addEventListener('click', function(e) {
-        // Reset arrays before loading data from local storage
-        pantry.length = 0;
-        recipes.length = 0;        
-
-        const pantryData = JSON.parse(localStorage.getItem('pantryData'));
-        for(let ingredient of pantryData) {
-            addToPantry(ingredient.name, ingredient.qty, ingredient.unit);
+            localStorage.setItem('pantryData', pantryData);
+            localStorage.setItem('recipeData', recipeData);
         }
 
-        const recipeDataParse = JSON.parse(localStorage.getItem('recipeData'));
-        for (recipe of recipeDataParse) {
-            recipes.push(recipe);
+        if (e.target === loadData) {
+            // Reset arrays before loading data from local storage
+            pantry.length = 0;
+            recipes.length = 0;        
+
+            const pantryData = JSON.parse(localStorage.getItem('pantryData'));
+            for(let ingredient of pantryData) {
+                addToPantry(ingredient.name, ingredient.qty, ingredient.unit);
+            }
+
+            const recipeDataParse = JSON.parse(localStorage.getItem('recipeData'));
+            for (recipe of recipeDataParse) {
+                recipes.push(recipe);
+            }
         }
 
-        resetInputsExceptFor();
-        displayPantry();
-        displayRecipes();
-    });
+        if (e.target === eraseData) {
+            // Reset arrays before loading data from local storage
+            pantry.length = 0;
+            recipes.length = 0;    
+            
+            const pantryData = JSON.stringify(pantry);
+            const recipeData = JSON.stringify(recipes);
 
-    // Erase data from local storage and pantry/recipe arrays.
-    eraseData.addEventListener('click', function(e) {
-        // Reset arrays before loading data from local storage
-        pantry.length = 0;
-        recipes.length = 0;    
-        
-        const pantryData = JSON.stringify(pantry);
-        const recipeData = JSON.stringify(recipes);
-
-        localStorage.setItem('pantryData', pantryData);
-        localStorage.setItem('recipeData', recipeData);
-
-        resetInputsExceptFor();
-        displayPantry();
-        displayRecipes();
-    });
-
-
+            localStorage.setItem('pantryData', pantryData);
+            localStorage.setItem('recipeData', recipeData);
+        }
+            resetInputsExceptFor();
+            displayPantry();
+            displayRecipes();
+    }
 
     // **************FUNCTIONS*************
     function addToPantry(item, qty, unitOfMeasure) {
     
-        if(pantry.some(ingredient => ingredient.name.includes(item))) {
+        if (pantry.some(ingredient => ingredient.name.includes(item))) {
             // Lookup object name in pantry array
             for (let ingredient of pantry) {
-                if(ingredient.name === item) {
+                if (ingredient.name === item) {
                     ingredient.updateQty(qty);
                     // Prompt user to confirm unit of measurement if already exists in database.
-                    if(ingredient.unit !== unitOfMeasure) {
+                    if (ingredient.unit !== unitOfMeasure) {
                         let response = confirm(`The unit of measurement for "${ingredient.name}" already exists in the database. Do you want to update this from "${ingredient.unit}" to "${unitOfMeasure}"`);
-                        if(response) {
+                        if (response) {
                             ingredient.unit = unitOfMeasure;
                             alert(`The unit of measurement for ${ingredient.name} has been updated to "${unitOfMeasure}".`)
                         } else alert(`The unit of measurement for ${ingredient.name} was not updated.`)
@@ -366,9 +361,9 @@
         }
 
         for (let ingredient of pantry) {
-            if(ingredient.qty) {
-                if(search) {
-                    if(regex.test(ingredient.name)) {
+            if (ingredient.qty) {
+                if (search) {
+                    if (regex.test(ingredient.name)) {
                         let newListItem = document.createElement('li');
                         newListItem.innerHTML = `\n<span>• ${ingredient.name} ${ingredient.qty} ${ingredient.unit} </span>\n<a id="${ingredient.name}" class="button delete">Delete</a>\n<a class="button add-to-recipe">Add to recipe</a>\n`;
                         pantryList.appendChild(newListItem);
@@ -386,7 +381,7 @@
         ingredientsList.innerHTML = "";
 
         for (let ingredient of tempRecipe.ingredients) {
-            if(ingredient.qty){
+            if (ingredient.qty){
                 let newListItem = document.createElement('li');
                 newListItem.innerHTML = `\n<span>• ${ingredient.name} ${ingredient.qty} ${ingredient.unit} </span>\n<a id="${ingredient.name}" class="button delete">Delete</a>\n`;
                 ingredientsList.appendChild(newListItem);
@@ -407,8 +402,8 @@
 
 
         for (let recipe of recipes) {
-            if(search.length > 0) {
-                if(regex.test(recipe.name)) {
+            if (search.length > 0) {
+                if (regex.test(recipe.name)) {
                     let newListItem = document.createElement('li');
                     newListItem.innerHTML = `\n<span>• ${recipe.name} </span>\n<a class="button view-nutrition">Data</a>\n<a id="${recipe.name}" class="button delete">Delete</a>\n<a class="button view-recipe">View</a>\n`;
                     recipeList.appendChild(newListItem);
@@ -428,11 +423,11 @@
         const selects = Array.from(allSelects);
         
         inputs.forEach(input => {
-            if(! exceptions.includes(input.id)) input.value = "";
+            if (! exceptions.includes(input.id)) input.value = "";
         });
 
         selects.forEach(select => {
-            if(! exceptions.includes(select.id)) select.value = "";
+            if (! exceptions.includes(select.id)) select.value = "";
         });
     }
 
@@ -441,7 +436,7 @@
         // Regex array - 0 for min length 3 test, 1 for number test.
         const regex = [/.{3,}/gi, /^[+-]?\d*\.?\d+$/gi, /./gi ];
 
-        if(regex[expression].test(input)) {
+        if (regex[expression].test(input)) {
             el.classList.remove('invalid');
             el.setCustomValidity('');
             
@@ -461,7 +456,7 @@
         
         for (let ingredient of recipeLookup.ingredients) {
             
-            if(!ingredient.nutrition) {
+            if (!ingredient.nutrition) {
                 let ingredientToStr = `${ingredient.qty} ${ingredient.unit} ${ingredient.name}`;
                 let url = `${BASE_URL}app_id=${APP_ID}&app_key=${API_KEY}&nutrition-type=logging&ingr=${ingredientToStr}`;
 
@@ -488,7 +483,7 @@
                     document.getElementById('nutrition-data').appendChild(newListItem);
 
                     // let animateDisplay = setInterval(function() {
-                    //     // if(newListItem.style.opacity < 1) 
+                    //     // if (newListItem.style.opacity < 1) 
                     //     newListItem.style.opacity += parseFloat(0.1);
                     //     // else clearInterval(animateDisplay);
                     // }, 5000);
@@ -510,7 +505,7 @@
                 document.getElementById('nutrition-data').appendChild(newListItem);
 
                 // let animateDisplay = setInterval(() => {
-                //     // if(newListItem.style.opacity < 1) 
+                //     // if (newListItem.style.opacity < 1) 
                 //     newListItem.style.opacity += 0.1;
                 //     // else clearInterval(animateDisplay);
                 // }, 500);
